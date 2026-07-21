a735ae203c0f6e28749aab75b53bcc499e13ec4a
Sara Jafari <sarajafary26@gmai.com>
2026-07-21 16:18:59 +0330

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+import Product from './classes/Product.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let close = document.querySelectorAll('.close');
+    let modal = document.querySelector('.modal-keeper');
+    let addProduct = document.querySelector('.add-product');
+    let create = document.querySelector('.create');
+    let title = document.querySelector('.title');
+    let price = document.querySelector('.price');
+    let image = document.querySelector('.image');
+    let background = document.querySelector('.background');
+    let productList = document.querySelector('.product-list');
+
+    const ProductClass = new Product(productList, modal);
+
+    close.forEach(btn => {
+        btn.addEventListener('click', () => ProductClass.closeForm());
+    });
+
+    addProduct.addEventListener('click', () => ProductClass.openForm());
+
+    create.addEventListener('click', createProduct);
+
+    function createProduct() {
+        ProductClass.create(title, price, image, background);
+    }
+});
---
+++ src/js/classes/Product.js
@@ -0,0 +1,67 @@
+class Product {
+    constructor(productList, modal) {
+        this.productList = productList;
+        this.modal = modal;
+    }
+
+    create(title, price, image, background) {
+        let titleval = title.value;
+        let priceVal = price.value;
+        let imageVal = URL.createObjectURL(image.files[0]);
+        let backgroundVal = background.value;
+
+        const colors = {
+            purple: "bg-purple-500",
+            yellow: "bg-yellow-500",
+            teal: "bg-teal-500",
+            orange: "bg-orange-500",
+            red: "bg-red-500",
+            blue: "bg-blue-500",
+            pink: "bg-pink-500",
+        };
+
+        let bgClass = colors[backgroundVal];
+
+        let ProductCard = `<div class="productCard ${bgClass}">
+                <svg class="absolute bottom-0 left-0 mb-8" viewBox="0 0 375 283" fill="none"
+                    style="transform: scale(1.5); opacity: 0.1;">
+                    <rect x="159.52" y="175" width="152" height="152" rx="8" transform="rotate(-45 159.52 175)"
+                        fill="white" />
+                    <rect y="107.48" width="152" height="152" rx="8" transform="rotate(-45 0 107.48)" fill="white" />
+                </svg>
+
+                <div class="relative pt-10 px-10 flex items-center justify-center">
+                    <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                        style="background: radial-gradient(black, transparent 60%); transform: rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1); opacity: 0.2;">
+                    </div>
+
+                    <img class="relative w-40"
+                        src="${imageVal}"
+                        alt="">
+                </div>
+
+                <div class="relative text-white px-6 pb-6 mt-6">
+                    <span class="block opacity-75 -mb-1">Outdoor</span>
+                    <div class="flex justify-between">
+                        <span class="block font-semibold text-xl">${titleval}</span>
+                        <span
+                            class="block bg-white rounded-full text-teal-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$${priceVal}</span>
+                    </div>
+                </div>
+            </div>`;
+
+        this.productList.innerHTML = ProductCard + this.productList.innerHTML;
+
+        this.closeForm();
+    }
+
+    openForm() {
+        this.modal.classList.remove('hidden');
+    }
+
+    closeForm() {
+        this.modal.classList.add('hidden');
+    }
+}
+
+export default Product;
